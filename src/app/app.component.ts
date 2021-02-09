21342f78280fc9387a41bb00b64eca65337ec762
manjula10 <manjula@skills-agency.com>
2021-02-09 11:33:08 +0530

    update once again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { DataTableDirective } from 'angular-datatables';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import  *  as  data  from  './../assets/data/MOCK_DATA.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,87 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qnap';
+  @ViewChild(DataTableDirective)
+  datatableElement: DataTableDirective;    
+  dtOptions: DataTables.Settings = {};
+  fileInfo:any=[];
+  formated_data :any=[];
+  filterForm: FormGroup;
+  filterVal:any;
+ 
+  constructor(private fb: FormBuilder ){}
+  ngOnInit(){
+    let fileInformation:any = data;
+    this.fileInfo = fileInformation.default; 
+    
+    this.filterForm = this.fb.group({
+      filterVal: null
+    });
+
+    this.dataTableCall();
+  }
+
+  public dataTableCall(filterVal: any = '', closed_files: any = '') {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      responsive: true,
+      searching: false,
+      lengthChange: false,
+      serverSide: true,
+      processing: true,
+      order: [],
+      columnDefs: [
+        {
+          "targets": 'nosort',
+          "orderable": false
+        }
+      ],
+      ajax: (dataTablesParameters: any, callback:any) => {         
+        this.formated_data = this.filterfileData(dataTablesParameters);
+          
+        callback({
+          recordsTotal: 200 ,
+          recordsFiltered: 200,
+          data: []
+        }); 
+      },
+      columns: [{ data: 'id' }, { data: 'first_name' }, { data: 'last_name' }, { data: 'email' }
+        , { data: 'gender' }, { data: 'ip_address' }, { data: 'timestamp' }]
+    };
+  }
+
+  filterfileData(dataparam){
+    let start=0;
+    let length = dataparam.length;
+
+    let filterData = this.fileInfo.splice(start, length);
+    return filterData;
+  }
+
+
+  onchangeFilter($event) { 
+    let filterVal = $event.srcElement.value; 
+    console.log(filterVal, " filterVal ");
+    
+    if(filterVal != ''){
+      this.formated_data = this.formated_data.find(item => (item.id === filterVal || item.first_name === filterVal || item.last_name === filterVal || item.email === filterVal || item.gender === filterVal));
+      
+      console.log(this.formated_data, " this.formated_data ");
+      
+      if(this.formated_data && this.formated_data.length>0){
+        this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+          dtInstance.draw();
+        });
+      }
+    } else {
+      this.formated_data = this.fileInfo;
+      this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.draw();
+      });
+    }
+    
+    
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataTablesModule } from 'angular-datatables';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule,
+    ReactiveFormsModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
